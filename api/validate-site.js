50c1d82e6a7db25ab27b707a9384f6730b5c89ac
ychrisp <32323075+ychrisp@users.noreply.github.com>
2024-01-03 10:13:28 +0800

    Update validate-site.js

--- api/validate-site.js
+++ api/validate-site.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    const isIpCheckEnabled = process.env.IS_IPCHECK_ING === 'ipcheck.ing';
+    const isIpCheckEnabled = process.env.IS_IPCHECK_ING === 'ipchick.com';
     res.status(200).json({ isIpCheckEnabled });
 };
